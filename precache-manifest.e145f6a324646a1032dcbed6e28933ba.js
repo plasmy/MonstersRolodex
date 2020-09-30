69a5a3c0708b44ec83362f3f91d292068c4a7d8b
Manuel Santos <mariostar1@gmail.com>
2020-09-30 13:59:59 -0400

    Updates

---
+++ precache-manifest.e145f6a324646a1032dcbed6e28933ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3077b1d430ec989b569556ef036cacec",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "b4f1688c2538b6146df3",
+    "url": "/MonstersRolodex/static/css/main.cf90c6f9.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/MonstersRolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersRolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4f1688c2538b6146df3",
+    "url": "/MonstersRolodex/static/js/main.15012b7f.chunk.js"
+  },
+  {
+    "revision": "425f1fd85b5840b582ed",
+    "url": "/MonstersRolodex/static/js/runtime-main.2b75c539.js"
+  }
+]);
